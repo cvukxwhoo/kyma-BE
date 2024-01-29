9f0392125e91b6090bb70a7ce340567bf6596d7c
cvukxwhoo <chienvuk@gmail.com>
2024-01-30 00:08:06 +0700

    submit form

--- controllers/billController.js
+++ controllers/billController.js
@@ -1,25 +1,46 @@
-import { StatusCodes } from "http-status-codes";
-import BillModel from "../models/Bill.js";
+import { StatusCodes } from 'http-status-codes';
+import BillModel from '../models/Bill.js';
+import UserModel from '../models/User.js';
+import ProductModel from '../models/Product.js';
 
 const billController = {
   createBill: async (req, res) => {
     try {
-      const createBill = req.body;
+      const { formData, productIds } = req.body;
 
-      //   Create Brands
-      const newBill = await BillModel.create(createBill);
-      // Save the new product to the database
-      const saveBill = await newBill.save();
-      res.status(StatusCodes.CREATED).json({
-        message: "Submit bill successfully!",
-        data: saveBill,
+      // Find the user and products using their IDs
+
+      const userId = req.cookies.userId;
+      const user = await UserModel.findById(userId);
+
+      if (!user) {
+        return res
+          .status(StatusCodes.NOT_FOUND)
+          .json({ error: 'User not found' });
+      }
+
+      // Create an array of product objects with quantity
+      const orders = productIds.map((productId) => ({
+        product: productId,
+        quantity: 1, // Set the default quantity, you can adjust as needed
+      }));
+
+      // Create a new order using the Order model and populate the user and products fields
+      const newOrder = new BillModel({
+        formData,
+        user,
+        orders,
       });
+
+      // Save the order to the database
+      const savedOrder = await newOrder.save();
+
+      res.status(StatusCodes.CREATED).json(savedOrder);
     } catch (error) {
-      console.error("Error submit bill:", error);
-      res.status(StatusCodes.BAD_REQUEST).json({
-        message: "Error sibmit bill",
-        error: error.message, // Include the error message in the response
-      });
+      console.error('Error creating order:', error);
+      res
+        .status(StatusCodes.INTERNAL_SERVER_ERROR)
+        .json({ error: 'Internal Server Error' });
     }
   },
 };
--- models/Bill.js
+++ models/Bill.js
@@ -1,49 +1,21 @@
-import { format } from "date-fns";
-import mongoose from "mongoose";
+import { format } from 'date-fns';
+import mongoose from 'mongoose';
 
 const BillSchema = mongoose.Schema({
-  customer: {
-    type: mongoose.Schema.Types.ObjectId,
-    ref: "users",
-    required: true,
-  },
-  email: { type: String, required: true },
-  fullName: { type: String, required: true },
-  phoneNumber: {
-    type: Number,
-    require: true,
-  },
-  address: {
-    type: String,
-    require: true,
-  },
-  invoice: {
-    type: String,
-    require: true,
-  },
-  companyName: {
-    type: String,
-    require: false,
-  },
-  taxCode: {
-    type: String,
-    require: false,
-  },
-  addressCompany: {
-    type: String,
-    require: false,
-  },
-  textareaContent: {
-    type: String,
-    require: false,
-  },
-
-  createdAt: {
-    type: String,
-    default: format(new Date(), "MMM dd, yyyy"),
-  },
+  formData: { type: Object, required: true },
+  user: { type: mongoose.Schema.Types.ObjectId, ref: 'User', required: true },
+  products: [
+    {
+      product: {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: 'Product',
+        required: true,
+      },
+      quantity: { type: Number, required: true },
+    },
+  ],
 });
 
-const BillModel = mongoose.model("bills", BillSchema);
+const BillModel = mongoose.model('bills', BillSchema);
 
 export default BillModel;
